83574d3e1527426158e5ca65fc3f259822a07bf0
Neo3691102 <nestor8772@gmail.com>
2025-12-05 15:08:28 -0600

    Se crea la clase Libro que es un POJO donde se muestran datos de un libro.

--- capadatos/src/main/java/CapaDatos.java
+++ capadatos/src/main/java/CapaDatos.java
@@ -119,3 +119,59 @@ class Autor{
     }
 }
 
+class Libro{
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+    private int ISBN;
+
+    public Libro(String titulo, String autor, int anioPublicacion, int ISBN){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anioPublicacion=" + anioPublicacion +
+                ", ISBN=" + ISBN +
+                '}';
+    }
+}
+
