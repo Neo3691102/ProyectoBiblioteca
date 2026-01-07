329df486f7779a0ed2ca6d5e4a1a206ab92a48cd
Neo3691102 <nestor8772@gmail.com>
2026-01-06 21:27:04 -0600

    Se delega la creacion de usuario solo para la clase contexto y se delega el guardado a traves de las capas de negocio y de datos.

--- capadatos/src/main/java/capadatos/CapaDatos.java
+++ capadatos/src/main/java/capadatos/CapaDatos.java
@@ -1,9 +1,34 @@
 package capadatos;
 
+
+import capadatos.dtoEntidades.Usuario;
+import capadatos.model.UsuarioModel;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class CapaDatos {
-    public String infoUsuario(Usuario usuario){
-        return usuario.toString();
+    public String infoUsuario(UsuarioEjemplo usuarioEjemplo){
+        return usuarioEjemplo.toString();
     }
+
+    public void guardarUsuarioCP(Usuario usuario){
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("coneccionLocalMySQL");
+        EntityManager em = emf.createEntityManager();
+
+
+        UsuarioModel um = new UsuarioModel(em);
+
+
+        um.guardar(usuario);
+
+
+        em.close();
+        emf.close();
+
+    }
+
 }
 
 
